fe76f9a690b5e48a770797ea4591b99d7f6950c7
Adrian Gligor <adrian.gligor@gmail.com>
2013-03-09 18:32:53 +0100

    added interactive shell (repl)

--- stage1/src/shell.c
+++ stage1/src/shell.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "cpu.h"
 #include "parser.h"
 
@@ -17,22 +18,59 @@ void shell_print_state(const cpu_state *state)
 }
 
 /**
+ * execute a line from a command listing
+ */
+void shell_file(const char *filename)
+{
+    cpu_state state;
+    cpu_reset(&state);
+    
+    parse_instr(filename, &state.instr);
+    cpu_exec(&state);
+    shell_print_state(&state);
+
+    return;
+}
+
+/**
+ * execute a line from stdin
+ */
+void shell_stdin()
+{
+    cpu_state state;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    while ((read = getline(&line, &len, stdin)) != -1) {
+        cpu_reset(&state);
+
+        parse_instr_line(line, &state.instr);
+        cpu_exec(&state);
+        shell_print_state(&state);
+    }
+
+    return;
+}
+
+/**
  * initializes the vcpu state and starts execution
  */
 int main(const int argc, const char *argv[])
 {
     if (argc < 2) {
-        printf("usage: %s file.lst\n", argv[0]);
+        printf("usage: %s -\n", argv[0]);
+        printf("       %s file.lst\n", argv[0]);
         exit(1);
     }
+    
+    const char *filename = argv[1];
+    
+    if (strncmp(filename, "-", 2) == 0) {
+        shell_stdin();
+    } else {
+        shell_file(filename);
+    }
 
-    cpu_state state;
-    cpu_reset(&state);
-
-    parse_instr(argv[1], &state.instr);
-
-    cpu_exec(&state);
-
-    shell_print_state(&state);
     return(0);
 }
